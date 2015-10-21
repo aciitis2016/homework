9e37eee32b083c969a8b363b4cca4b66860455fa
ayratn <nantuko84@gmail.com>
2015-10-21 17:55:19 +0300

    Added Main.java and task5.jsp

---
+++ src/edu/kzn/itis/Main.java
@@ -0,0 +1,8 @@
+package edu.kzn.itis;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
---
+++ src/edu/kzn/itis/jsp/task5.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+  <h1>Hello, JSP/GIT/ITIS!</h1>
+</body>
+</html>
